fb252dc859036a4926741836d6dbe7df3f77d998
Yohan Bustos <yohansebastianbr@gmail.com>
2024-10-27 17:49:52 -0500

    Creacion de los endpoints para autenticarte y para deslogearte

--- src/main/java/com/example/my/personal/blog/controller/UserController.java
+++ src/main/java/com/example/my/personal/blog/controller/UserController.java
@@ -2,12 +2,16 @@ package com.example.my.personal.blog.controller;
 
 import com.example.my.personal.blog.persistence.entity.UserEntity;
 import com.example.my.personal.blog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -34,6 +38,29 @@ public class UserController {
     public String registe(UserEntity user){
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/access")
+    public String access(HttpSession session) {
+        Optional<UserEntity> optionalUser = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString()));
+
+        if (optionalUser.isPresent()) {
+            session.setAttribute("user_session_id", optionalUser.get().getId());
+            return "redirect:/posts/home";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/login";
     }
 
